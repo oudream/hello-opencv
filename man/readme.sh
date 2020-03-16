f9211767b532d456640d91b97205674b22cf0897
oudream <oudream@126.com>
2020-03-16 16:55:55 +0800

    Update readme.sh

---
+++ man/readme.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+# https://github.com/opencv/opencv/wiki
+# https://docs.opencv.org/4.1.2/d9/df8/tutorial_root.html
+# https://www.tutorialspoint.com/opencv/opencv_quick_guide.htm
+
+
+# examples c++
+# https://github.com/spmallick/learnopencv
+# https://github.com/oreillymedia/Learning-OpenCV-3_examples
+
+
+# mat
+# https://docs.opencv.org/4.1.2/d3/d63/classcv_1_1Mat.html
