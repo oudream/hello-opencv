b72ad4520011bbbf2b683c423d7ff67ccb45d1d2
oudream <oudream@126.com>
2020-03-25 16:17:49 +0800

    add man build and cpp

--- man/b.cpp
+++ man/b.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 using namespace cv;
 
+void fn_outFile(const cv::Mat & m, const string & fp);
+
 std::string f_fpExec, f_paBin, f_paOs, f_paDeploy;
 
 int main(int argc, char *argv[])
@@ -15,7 +17,41 @@ int main(int argc, char *argv[])
     f_paBin = f_fpExec.find_last_of("/\\") != std::string::npos ? f_fpExec.substr(0, f_fpExec.find_last_of("/\\")) : std::string();
     f_paOs = f_paBin.find_last_of("/\\") != std::string::npos ? f_paBin.substr(0, f_paBin.find_last_of("/\\")) : std::string();
     f_paDeploy = f_paOs.find_last_of("/\\") != std::string::npos ? f_paOs.substr(0, f_paOs.find_last_of("/\\")) : std::string();
-    std::cout << "f_paDeploy: " << f_fpExec << std::endl;
+    cout << "f_paDeploy: " << f_fpExec << endl;
+
+    // zero
+    cv::Size(1, 49), CV_16SC3, Scalar(0));
+
+    // Write to file!
+    cv::FileStorage file("some_name.ext", cv::FileStorage::WRITE);
+    cv::Mat someMatrixOfAnyType;
+    file << "matName" << someMatrixOfAnyType;
 
     return 0;
+}
+
+void fn_outFile(const cv::Mat & m, const string & fp)
+{
+    //使用ofstream来输出mat
+    std::ofstream fs(fp);
+    if( !fs.is_open() )
+    {
+        std::cout<<"open file error!"<<std::endl;
+        return;
+    }
+
+    //循环输出
+    int height = m.rows;
+    int width = m.cols;
+    for( int i=0; i<height; i++ )
+    {
+        for( int j=0; j<width; j++ )
+        {
+            fs << (int)m.ptr<uchar>(i)[j] << '\t';
+            //不加类型转换用txt打开是字符
+        }
+        fs << std::endl;
+    }
+
+    fs.close();
 }
