392b531e560babea4029cc1406b41103162fa9a3
oudream <oudream@126.com>
2020-03-25 16:19:50 +0800

    add examples line detector and canny

---
+++ examples/hough/line_detector_frotms/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char *argv[])
+{
+    //创建一个2*2 类型为8位的uchar类型三通道的 颜色为黄色
+    Mat img(2, 2, CV_8UC3, Scalar(0, 255, 255));
+    cout << "矩阵元素" << endl << img << endl;
+
+    /*Mat srcImage = imread("G:\\group.jpg");
+    cout << "图片矩阵" << endl << srcImage << endl;*/
+
+    return 0;
+}
